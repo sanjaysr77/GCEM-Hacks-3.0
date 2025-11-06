0b8e6ae8e1cd26d4be98362f9e129e3fe22babf2
Jyesht <jyesht147@gmail.com>
2025-11-06 19:36:55 +0530

    apreciation

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <p>Welcome to the home page.</p>
+      <p>Welcome to the home page. CHALLA KI JAI</p>
     </div>
   );
 }
